47c7a36110a1d726c8254a31ec4a79add5251f11
Jiuyang Liu <liujiuyang1994@gmail.com>
2021-01-26 15:02:27 +0000

    remove void bracket.

--- diplomacy/src/main/scala/Nodes.scala
+++ diplomacy/src/main/scala/Nodes.scala
@@ -1680,7 +1680,7 @@ class AdapterNode[D, U, EO, EI, B <: Data](
   *
   * During hardware generation, [[IdentityNode]]s automatically connect their inputs to outputs.
   */
-class IdentityNode[D, U, EO, EI, B <: Data](imp: NodeImp[D, U, EO, EI, B])()(implicit valName: sourcecode.Name)
+class IdentityNode[D, U, EO, EI, B <: Data](imp: NodeImp[D, U, EO, EI, B])(implicit valName: sourcecode.Name)
     extends AdapterNode(imp)({ s => s }, { s => s }) {
   override def description = "identity"
   override final def circuitIdentity = true
@@ -1695,7 +1695,7 @@ class IdentityNode[D, U, EO, EI, B <: Data](imp: NodeImp[D, U, EO, EI, B])()(imp
   * An ephemeral node provides a mechanism to directly connect two nodes to each other where neither node knows about the other,
   * but both know about an ephemeral node they can use to facilitate the connection.
   */
-class EphemeralNode[D, U, EO, EI, B <: Data](imp: NodeImp[D, U, EO, EI, B])()(implicit valName: sourcecode.Name)
+class EphemeralNode[D, U, EO, EI, B <: Data](imp: NodeImp[D, U, EO, EI, B])(implicit valName: sourcecode.Name)
     extends AdapterNode(imp)({ s => s }, { s => s }) {
   override def description = "ephemeral"
   override final def circuitIdentity = true
